fb4a67933061c1ebf7644ac5c1300b806a1e39f8
TomasMertian <mertiantomas@gmail.com>
2026-09-06 19:30:17 -0300

    feat: resuelvo ejercicio parte 1 nro 9

--- src/parte 1/ejercicios.ts
+++ src/parte 1/ejercicios.ts
@@ -133,8 +133,7 @@ export function buscarPorNombre(
 // Devolver true si existe al menos un alumno con nota menor a 6.
 // Resolver utilizando some.
 export function existeDesaprobado(alumnos: Alumno[]): boolean {
-    // TODO
-    throw new Error("Implementar");
+    return alumnos.some((alumno) => alumno.nota < 6);
 }
 
 // -----------------------------------------------------------------------------
